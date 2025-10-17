52657ae66bdcd6df8c984530a942cb5ef75a9799
Haile Dagne <haile.dange0@gmail.com>
2025-10-16 22:29:17 -0400

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,45 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     host: true,
+//     strictPort: false,
+//     cors: true,
+//     headers: {
+//       "Cache-Control": "no-cache, no-store, must-revalidate",
+//       Pragma: "no-cache",
+//       Expires: "0",
+//     },
+//     middlewareMode: false,
+//     hmr: {
+//       overlay: true,
+//     },
+//   },
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks: undefined,
+//       },
+//     },
+//   },
+//   optimizeDeps: {
+//     include: ["react", "react-dom", "react-router-dom"],
+//     force: true,
+//   },
+//   esbuild: {
+//     jsx: "automatic",
+//   },
+// });
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    host: true,
-    strictPort: false,
-    cors: true,
-    headers: {
-      "Cache-Control": "no-cache, no-store, must-revalidate",
-      Pragma: "no-cache",
-      Expires: "0",
-    },
-    middlewareMode: false,
-    hmr: {
-      overlay: true,
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ["react", "react-dom", "react-router-dom"],
-    force: true,
-  },
-  esbuild: {
-    jsx: "automatic",
-  },
+  base: "/Abity/",
 });
