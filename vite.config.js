01ee601e8236a070a2beac20236bdd90d08f3336
Haile Dagne <haile.dange0@gmail.com>
2025-10-16 23:15:24 -0400

    Final

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FoodMenu/", // This should match your GitHub repository name
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-  },
-  server: {
-    port: 5173,
-    host: true,
-  },
+  base: "/Abity/",
 });
